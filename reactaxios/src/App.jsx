53f001ef5787bd6f4ddbbad47101f78037c93af7
fardeen <fardeenkhan63574@gmail.com>
2025-11-16 14:10:24 +0530

    crud

---
+++ reactaxios/src/App.jsx
@@ -0,0 +1,145 @@
+import { useEffect } from 'react'
+import './App.css'
+import { Dltapi, Getapi, Postapi } from './services/axios'
+import { useState } from 'react'
+
+
+function App() {
+  const [data,setdata]=useState([])
+
+  const getdataapi=async()=>{
+  let res=await Getapi()
+  if(res.status===200){
+
+setdata(res.data)
+
+
+
+  }
+
+
+  }
+// console.log(data)
+useEffect(()=>{
+getdataapi()
+},[])
+
+const handledlt=async(id)=>{
+  try {
+    
+    let resp=await Dltapi(id)
+    console.log(resp)
+    
+    setdata((prev)=>prev.filter((todo)=>{
+    return(
+      todo.id!==id
+    )
+    
+    }))
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+const[form,setform]=useState({
+title:"",
+body:""
+
+})
+const handleform=(e)=>{
+
+let{name,value}=e.target
+
+
+setform((prev)=>({...prev,[name]:value}))
+
+
+}
+
+let getpostapi=async()=>{
+  try {
+    let res= await Postapi(form)
+if(res.status===201){
+  setdata((prev)=>[...prev,res.data])
+  setform({title:"",body:""})
+}    
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+// console.log(data)
+const submit=(e)=>{
+ e.preventDefault()
+
+  if (!form.title.trim() || !form.body.trim()) return;
+
+
+ 
+ getpostapi()
+
+
+
+}
+const handledit=(ele)=>{
+setform({
+  title:ele.title,
+  body:ele.body
+})
+
+
+}
+
+  return (
+    <>
+<main>
+
+
+<form onSubmit={submit}>
+
+<input type="text"
+placeholder='write...'
+name="title"
+value={form.title}
+onChange={(e)=>handleform(e)}
+/>
+<input type="text"
+placeholder='about...'
+name="body"
+value={form.body}
+onChange={(e)=>handleform(e)}
+/>
+<button>add</button>
+
+
+</form>
+
+  <ol>
+{data.map((ele)=>{
+  const{id,body,title}=ele
+return(
+<li key={id}>
+
+<p className="f">{title}</p>
+<p className="s">{body}</p>
+<button onClick={()=>handledit(ele)}>edit</button>
+<button onClick={()=>handledlt(id)}>delete</button>
+
+</li>
+
+
+)
+
+
+
+})}
+
+  </ol>
+</main>
+
+    </>
+  )
+}
+
+export default App
---
+++ reactaxios/src/services/axios.jsx
@@ -0,0 +1,32 @@
+
+import axios from "axios"
+
+
+const Api=axios.create({
+
+    baseURL:'https://jsonplaceholder.typicode.com'
+
+})
+
+export const Getapi=()=>{
+return(
+    Api.get("/posts")
+)
+
+
+}
+export const Dltapi=(id)=>{
+return(
+Api.delete(`/posts/${id}`)
+)
+
+}
+
+export const Postapi=(post)=>{
+
+return(
+Api.post("/posts",post)
+
+)
+
+}
